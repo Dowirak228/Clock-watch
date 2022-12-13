1959c82c2eee6851ab4a077bf26c96788ca76f5c
Nodir <istinniydowik228@gmail.com>
2022-12-13 18:33:31 +0500

    feat: Start js interactions

---
+++ code/script.js
@@ -0,0 +1,20 @@
+const clock = document.querySelector(".clock__watch");
+
+const startBtn = document.querySelector("#start");
+const pauseBtn = document.querySelector("#pause");
+const resetBtn = document.querySelector("#reset");
+
+
+
+
+startBtn.addEventListener("click", () => {
+
+});
+
+pauseBtn.addEventListener("click", () => {
+
+});
+
+resetBtn.addEventListener("click", () => {
+
+});
